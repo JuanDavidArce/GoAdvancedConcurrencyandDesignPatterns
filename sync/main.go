f7c27e0fdf3e4d5645b627601178a500dd5e9390
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-03-31 16:46:11 -0500

    RWMutex RLock and WLock

--- sync/main.go
+++ sync/main.go
@@ -7,7 +7,7 @@ import (
 
 var balance int = 0
 
-func Deposit(amount int, wg *sync.WaitGroup, lock *sync.Mutex) {
+func Deposit(amount int, wg *sync.WaitGroup, lock *sync.RWMutex) {
 	defer wg.Done()
 	lock.Lock()
 	b := balance
@@ -15,19 +15,24 @@ func Deposit(amount int, wg *sync.WaitGroup, lock *sync.Mutex) {
 	lock.Unlock()
 }
 
-func Balance() int {
+func Balance(lock *sync.RWMutex) int {
+	lock.RLock()
 	b := balance
+	lock.RUnlock()
 	return b
 }
 
+// 1 Deposit()
+// N Balance()
+
 func main() {
 	var wg sync.WaitGroup
-	var lock sync.Mutex
+	var lock sync.RWMutex
 	for i := 1; i <= 500; i++ {
 		wg.Add(1)
 		go Deposit(1, &wg, &lock)
 	}
 
 	wg.Wait()
-	fmt.Println(Balance())
+	fmt.Println(Balance(&lock))
 }
