3a62ea3f7d33deefbd3bf9c9f4e796c8cf59e914
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-03-31 22:41:38 -0500

    Port scaner without concurrency

---
+++ net/port.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	// Escanear cada puerto y hacer una conexión
+	for i := 0; i < 100; i++ {
+		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
+		if err != nil {
+			continue
+		}
+		conn.Close()
+		fmt.Println("Port", i, "is open")
+	}
+}
