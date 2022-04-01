9cadee17b40fa69169cd131f5fa491e8ccd4e645
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-03-31 22:50:37 -0500

    Port scaner  concurrency CLI

--- net/port.go
+++ net/port.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"sync"
 )
 
+var site = flag.String("site", "scanme.nmap.org", "url to scan")
+
 func main() {
-	// Escanear cada puerto y hacer una conexión
-	for i := 0; i < 100; i++ {
-		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
-		if err != nil {
-			continue
-		}
-		conn.Close()
-		fmt.Println("Port", i, "is open")
+	flag.Parse()
+	var wg sync.WaitGroup
+	for i := 0; i < 65535; i++ {
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *site, port))
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("Port %d is open\n", port)
+		}(i)
 	}
+	wg.Wait()
 }
